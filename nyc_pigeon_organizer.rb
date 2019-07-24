72c9be1544804060d9955447a9d1c694fb7e4e4c
optimistic-writer-7178 <duot.jim@gmail.com>
2019-07-24 17:38:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pig = Hash.new
+
+  data.each_pair do |general_attr, gval|
+    gval.each_pair do |specific_attr, names|
+      names.each do |name|
+        new_pig[name] = {} unless new_pig[name]
+        new_pig[name][general_attr] = [] unless new_pig[name][general_attr]
+        new_pig[name][general_attr] << specific_attr.to_s
+      end
+    end
+  end
+  new_pig
 end
